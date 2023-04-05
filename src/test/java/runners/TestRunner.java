395f70e60ae46bb0ddf62a94c1fda5d7a81e5ee0
dozerozturk <ozturkozer@yahoo.com>
2023-04-05 06:32:26 -0500

    update

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "step_definitions",
         dryRun = false,
-        tags = "@login",
+        tags = "",
         publish = false
 )
 
